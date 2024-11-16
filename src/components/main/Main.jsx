4842ac5e74e040c7ef3b6f3ad646d0457907be14
amar <amarkodge@gmail.com>
2024-11-16 22:37:52 +0530

    creating main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,12 @@
 import "./Main.css";
 
 function Main() {
-  return <section className="main"></section>;
+  return (
+    <section className="main-content">
+      <main className="main"></main>
+      <aside className="side-new"></aside>
+    </section>
+  );
 }
 
 export default Main;
